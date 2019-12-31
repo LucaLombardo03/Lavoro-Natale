fd92fb95d394fd67c7326f74b38ced90681bd14c
luca lombardo <lucalombardo03@gmail.com>
2019-12-31 13:53:36 +0100

    aggiunto commento righe 15-18

--- Lavoro_di_Natale/Program.cs
+++ Lavoro_di_Natale/Program.cs
@@ -6,27 +6,27 @@ namespace Lavoro_di_Natale
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
+            int a = int.MaxValue;
+            Console.WriteLine(a); //2^31 perchè il valore massimo di un int è 2^31-1 (un bit è per il segno) uguale 2.147.483.647;
+            while (a != int.MaxValue)
             {
-                int a = int.MaxValue;
-                Console.WriteLine(a); //2^31 perchè il valore massimo di un int è 2^31-1 (un bit è per il segno) uguale 2.147.483.647;
-                Console.WriteLine(a+1); //qua il codice trova un errore perchè il numero a va in overflow e quindi stampa l'opposto aggiungendo 1 = -2^31;
-                int b = -128;
-                int c = -b; 
-                Console.WriteLine(Convert.ToString(b, 2)); 
-                Console.WriteLine(Convert.ToString(c, 2));
-                int value = 31;
-                int somma = value;
-                value = ~value + 1; //NOT
-                //value = value + 1;
-                somma = somma + value;
-                Console.WriteLine($"{value} e {somma}"); 
-                long number = 55555;
-                Console.WriteLine(Convert.ToString(number, 2));
-                short number1 = (short)number;
-                Console.WriteLine(number1);
+                Console.WriteLine(a + 1); //qua il codice trova un errore perchè il numero a va in overflow e quindi stampa l'opposto aggiungendo 1 quindi -2^31;
             }
-
+            int b = -128; //viene assegnato un valore alla variabile b;
+            int c = -b;  //alla variabile c viene assegnato il valore dell'opposto della variabile b;
+            Console.WriteLine(Convert.ToString(b, 2)); //si converte in binario il valore della variabile b;
+            Console.WriteLine(Convert.ToString(c, 2)); //si converte in binario il valore della variabile c;
+            int value = 31;
+            int somma = value;
+            value = ~value + 1; //NOT
+            //value = value + 1;
+            somma = somma + value;
+            Console.WriteLine($"{value} e {somma}"); 
+            long number = 55555;
+            Console.WriteLine(Convert.ToString(number, 2));
+            short number1 = (short)number;
+            Console.WriteLine(number1);
+            Console.ReadLine();
         }
     }
 }
