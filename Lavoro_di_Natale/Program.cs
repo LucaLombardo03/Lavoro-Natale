52b40fb97a4feb01819532bab140eac5d32ea587
luca lombardo <lucalombardo03@gmail.com>
2020-01-02 17:30:58 +0100

    risposto al punto 10

--- Lavoro_di_Natale/Program.cs
+++ Lavoro_di_Natale/Program.cs
@@ -16,12 +16,13 @@ namespace Lavoro_di_Natale
             int c = -b;  //alla variabile c viene assegnato il valore dell'opposto della variabile b;
             Console.WriteLine(Convert.ToString(b, 2)); //si converte in binario il valore della variabile b;
             Console.WriteLine(Convert.ToString(c, 2)); //si converte in binario il valore della variabile c; 
-            int value = 31;
+            Console.WriteLine("Inserisci un numero intero: ");
+            int value = int.Parse(Console.ReadLine());
             int somma = value;
             value = ~value + 1; //NOT
             //value = value + 1;
             somma = somma + value;
-            Console.WriteLine($"{value} e {somma}"); 
+            Console.WriteLine($"{value} è il valore opposto da quello che ha inserito e {somma} è la somma tra il valore inserito e l'opposto"); 
             long number = 55555;
             Console.WriteLine(Convert.ToString(number, 2));
             short number1 = (short)number;
